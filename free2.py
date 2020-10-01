3186a88f658baa58c8086fbc9fbffcc678c4577b
zedone-m <72225832+zedone-m@users.noreply.github.com>
2020-10-01 21:02:10 +0200

    Add files via upload

---
+++ free2.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+import random
+import time
+import requests
+link="https://reward.ff.garena.com/?access_token="
+
+
+class Free:
+ def __init__(self,link,s,drive):
+   self.link=link
+   self.s=s
+   self.drive=drive
+   from getpass import getpass
+   token= getpass("enter the token: ")
+   link= link+token
+   time.sleep(2)
+ def randome():
+    myList=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9']
+    c=""
+    d=0
+    while d<4:
+      d=d+1
+      k=random.choice(myList)
+      c=c+k
+    return c
+ def exploiting():
+    driver = webdriver.Chrome(drive)
+    browser=driver.get(link)
+    time.sleep(4.2)
+    driver.refresh()
+    while True:
+     try:
+      a=Fire.randome()
+      b=Fire.randome()
+      c=Fire.randome()
+      driver.find_element_by_id("input_serial_1").send_keys(a)
+      driver.find_element_by_id("input_serial_2").send_keys(b)
+      driver.find_element_by_id("input_serial_3").send_keys(c)
+      driver.find_element_by_class_name("confirm-btn").click()
+      time.sleep(0.9)
+      driver.get(link)
+      time.sleep(1)
+     except KeyboardInterrupt:
+      print("you tried",s, "times")
+      quit()
+     except:
+      driver.quit()
+      exploiting(link,s)
+      print("a problem had popped out :")
+      print("you tried",s, "times")
+     s=s+1
+# exploiting(link,s)
---
+++ freefire.py
@@ -0,0 +1,87 @@
+from selenium import webdriver
+from getpass import getpass
+import requests
+import time
+from free2 import Free
+import random
+import re
+from free2 import Free
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+##########color#############
+def prRed(skk): 
+  print("\033[91m {}\033[00m" .format(skk)) 
+def prGreen(skk):
+ print("\033[92m {}\033[00m" .format(skk)) 
+def prYellow(skk): 
+  print("\033[93m {}\033[00m" .format(skk)) 
+def prLightPurple(skk): 
+  print("\033[94m {}\033[00m" .format(skk)) 
+def prPurple(skk):
+  print("\033[95m {}\033[00m" .format(skk)) 
+def prCyan(skk): 
+  print("\033[34m {}\033[00m" .format(skk)) 
+def prLightGray(skk): 
+  print("\033[97m {}\033[00m" .format(skk)) 
+def prBlack(skk): 
+  print("\033[98m {}\033[00m" .format(skk)) 
+prCyan('''
+    "welcome to my free fire cracking tool"
+''')
+prRed('''
+
+ ______ _______ ______         _____  __   _ _______
+  ____/ |______ |     \       |     | | \  | |______
+ /_____ |______ |_____/ _____ |_____| |  \_| |______
+                                                    
+
+''')
+
+class FacebookLogin():
+    def __init__(self, email, password, browser='Chrome'):
+        # Store credentials for login
+        self.email = email
+        self.password = password
+        if browser == 'Chrome':
+            # Use chrome
+            self.driver = webdriver.Chrome(drive)
+        elif browser == 'Firefox':
+            # Set it to Firefox
+            self.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        self.driver.get(LOGIN_URL)
+        time.sleep(1) # Wait for some time to load
+
+
+
+    def login(self):
+        email_element = self.driver.find_element_by_id('email')
+        email_element.send_keys(self.email) # Give keyboard input
+
+        password_element = self.driver.find_element_by_id('pass')
+        password_element.send_keys(self.password) # Give password as input too
+
+        login_button = self.driver.find_element_by_id('loginbutton')
+        login_button.click() # Send mouse click
+        time.sleep(6) # Wait for 2 seconds for the page to show up
+        ACCESS_URL= self.driver.current_url
+        self.driver.quit()
+        print(ACCESS_URL)
+        s=0
+        print("follow the video to add the access token:")
+        Free(ACCESS_URL,s,drive).exploiting()
+        
+if __name__ == '__main__':
+    email= input("input your email: ")
+    password= getpass("input your password: ")
+    drive=input("webdriver path: ")
+    # Enter your login credentials here
+    driver= webdriver.Chrome(drive)
+    browser=driver.get("https://reward.ff.garena.com/")
+    free_log=WebDriverWait(driver, 50).until(EC.visibility_of_element_located((By.XPATH, '//span[@class="btn-item facebook"]')))
+    free_log.click()
+    LOGIN_URL = driver.current_url
+    driver.quit()
+    fb_login = FacebookLogin(email, password, browser='Chrome')
+    fb_login.login()
