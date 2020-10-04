ea9f1da15dc9e638aa8ada0e53a108823f618db0
zedone-m <72225832+zedone-m@users.noreply.github.com>
2020-10-04 13:12:02 +0200

    Update freefire.py

--- freefire.py
+++ freefire.py
@@ -4,29 +4,32 @@ import requests
 import time
 from free2 import Free
 import random
-import re
+import re,os
 from free2 import Free
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+try:
+  os.system("clear")
+except:
+  pass
 ##########color#############
 def prRed(skk): 
-  print("\033[91m {}\033[00m" .format(skk)) 
+  print("\033[91m {}\033[00m" .format(skk),end="") 
 def prGreen(skk):
- print("\033[92m {}\033[00m" .format(skk)) 
+ print("\033[92m {}\033[00m" .format(skk),end="") 
 def prYellow(skk): 
-  print("\033[93m {}\033[00m" .format(skk)) 
+  print("\033[93m {}\033[00m" .format(skk),end="") 
 def prLightPurple(skk): 
-  print("\033[94m {}\033[00m" .format(skk)) 
+  print("\033[94m {}\033[00m" .format(skk),end="") 
 def prPurple(skk):
-  print("\033[95m {}\033[00m" .format(skk)) 
+  print("\033[95m {}\033[00m" .format(skk),end="") 
 def prCyan(skk): 
-  print("\033[34m {}\033[00m" .format(skk)) 
+  print("\033[34m {}\033[00m" .format(skk),end="") 
 def prLightGray(skk): 
-  print("\033[97m {}\033[00m" .format(skk)) 
+  print("\033[97m {}\033[00m" .format(skk),end="") 
 def prBlack(skk): 
-  print("\033[98m {}\033[00m" .format(skk)) 
+  print("\033[98m {}\033[00m" .format(skk),end="") 
 prCyan('''
     "welcome to my free fire cracking tool"
 ''')
@@ -67,17 +70,30 @@ class FacebookLogin():
         time.sleep(6) # Wait for 2 seconds for the page to show up
         ACCESS_URL= self.driver.current_url
         self.driver.quit()
+        prYellow('[-] ')
+        print('the url is', end= '')
+        prYellow(": ")
         print(ACCESS_URL)
         s=0
+        prYellow("[-] ")
         print("follow the video to add the access token:")
-        Free(ACCESS_URL,s,drive).exploiting()
+        token=getpass("enter the token: ")
+        Free(drive,token=token).exploiting()
         
 if __name__ == '__main__':
     email= input("input your email: ")
     password= getpass("input your password: ")
-    drive=input("webdriver path: ")
+    try:
+       with open("chromedriver.txt",'r') as f:
+          drive=(f.readlines())
+       driver= webdriver.Chrome(drive)
+    except:   
+       drive=input("webdriver path: ")
+       with open("chromedriver.txt",'w') as f:
+           f.writelines(drive)
+       driver= webdriver.Chrome(drive)
+
     # Enter your login credentials here
-    driver= webdriver.Chrome(drive)
     browser=driver.get("https://reward.ff.garena.com/")
     free_log=WebDriverWait(driver, 50).until(EC.visibility_of_element_located((By.XPATH, '//span[@class="btn-item facebook"]')))
     free_log.click()
