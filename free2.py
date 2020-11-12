6bde44f214b6751f2c7159f0711c7c86439eed4b
zedone-m <72225832+zedone-m@users.noreply.github.com>
2020-11-12 14:00:18 +0100

    Update free2.py

--- free2.py
+++ free2.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import random
 import time
 import requests
-
+import numpy as np
 s=0
 class Free:
  def __init__(self,drive,token,link="https://reward.ff.garena.com/?access_token="):
@@ -12,6 +12,7 @@ class Free:
    link= link+token
    self.link=link
    time.sleep(2)
+   self.li_st=np.arange(20000)
  def randome():
     myList=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9']
     c=""
@@ -21,7 +22,7 @@ class Free:
       k=random.choice(myList)
       c=c+k
     return c
- def exploiting(self):
+ def exploiting(self,X):
     driver = webdriver.Chrome(self.drive)
     browser=driver.get(self.link)
     time.sleep(4.2)
@@ -45,6 +46,9 @@ class Free:
       driver.quit()
       Free(self.drive,self.token).exploiting()
       print("a problem had popped out :")
+ def multiprocessing(self):
+      pool= Pool(5)
+      pool.imap(exploiting,self.li_st)
 #      print("you tried",s, "times")
 #     s=s+1
 # exploiting(link,s)
